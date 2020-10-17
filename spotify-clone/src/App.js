4f880ea1ca8d30aa4dd2bafd054936afab51e68b
shital@zestard.net <shital@zestard.net>
2020-10-18 01:51:18 +0530

    fetched users info

--- spotify-clone/src/App.js
+++ spotify-clone/src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-import Login from './Components/Login';
+import SpotifyWebAPi from "spotify-web-api-js";
+import Login from "./Components/Login";
+import Player from "./Components/Player";
+import { getTokenFromUrl } from "./API/spotify";
+
+const spotify = new SpotifyWebAPi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    const _token = hash.access_token;
+    console.log(_token);
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("user:-", user);
+      });
+    }
+  }, []);
+
   return (
     <div className="App">
-      
+      {token ? <Player /> : <Login />}
       {/* spotify logo */}
       {/* Login with spotify button */}
-      <Login />
     </div>
   );
 }
