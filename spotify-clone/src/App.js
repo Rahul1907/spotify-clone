5c4e2a04e509bdfb380431959d8d19cc63d9c3b4
shital@zestard.net <shital@zestard.net>
2020-10-17 15:49:43 +0530

    shopifylogin

---
+++ spotify-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Login from './Components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/* spotify logo */}
+      {/* Login with spotify button */}
+      <Login />
+    </div>
+  );
+}
+
+export default App;
